708533138f8de83e519a811c781d1fa60ebc05d1
Michael Von Wolfen <mihai.stoica98@hotmail.com>
2019-12-18 17:48:49 +0200

    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.

---
+++ Paranthesis-Validator/Paranthesis-Validator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Paranthesis_Validator
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length % 2 == 1)
+                return false;
+            Stack<char> brackets = new Stack<char>();
+            Dictionary<char, char> dicti = new Dictionary<char, char>();
+            dicti.Add('(', ')');
+            dicti.Add('[', ']');
+            dicti.Add('{', '}');
+            foreach (char c in s)
+            {
+                if (dicti.ContainsKey(c))
+                {
+                    brackets.Push(c);
+                    continue;
+                }
+                if (brackets.Count == 0)
+                    return false;
+                char a = (char)brackets.Pop();
+                if (dicti[a] != c)
+                    return false;
+            }
+            if (brackets.Count > 0)
+                return false;
+            else return true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().IsValid("{[][]{}()}"));
+        }
+    }
+}
