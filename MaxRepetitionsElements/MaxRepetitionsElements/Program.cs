7ac165035034a670157651edac6c11c7b0370408
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2021-10-05 00:45:22 +0300

    Started new algorithm

---
+++ MaxRepetitionsElements/MaxRepetitionsElements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MaxRepetitionsElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+            int[] numbers;
+            if(args.Length == 0)
+            {
+                //TODO Read all variables data from the console
+            }
+            else
+            {
+                //TODO Read all variables from args
+                if(args.Length !> 3)
+                {
+                    throw new Exception("You must include the length of the returned array and the array of elements to read")
+                }
+            }
+        }
+    }
+}
